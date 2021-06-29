d0b406ceef6f5b980fb2306c49d5a4bbbada6d71
konstantinaxt <axtkonstantin@hotmail.de>
2021-06-29 16:29:40 +0200

    add df21 favicons

--- pwa.mjs
+++ pwa.mjs
@@ -2,11 +2,11 @@ import pwaAssetGenerator from "pwa-asset-generator";
 
 (async () => {
   await pwaAssetGenerator.generateImages(
-    "./icons/21f.png",
+    "./icons/df21.jpeg",
     "./output_splashscreen",
     {
       scrape: false,
-      background: "#fffff",
+      background: "#CC8F63",
       splashOnly: true,
       portraitOnly: true
     }
